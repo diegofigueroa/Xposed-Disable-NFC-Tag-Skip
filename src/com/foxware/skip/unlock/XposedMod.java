b6a8d91d88e991442b99ebf0062d6efa79107d4f
Diego Figueroa <diegofigueroa99@gmail.com>
2013-12-09 20:38:43 -0600

    implements closing on skip uri

--- src/com/foxware/skip/unlock/XposedMod.java
+++ src/com/foxware/skip/unlock/XposedMod.java
@@ -1,8 +1,9 @@
-package com.mohammadag.disablenfctagempty;
+package com.foxware.skip.unlock;
 
 import java.util.List;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.nfc.NdefMessage;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -11,8 +12,7 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class XposedMod implements IXposedHookLoadPackage {
 	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
-		if (lpparam.packageName.equals("com.android.apps.tag")
-				|| lpparam.packageName.equals("com.google.android.tag")) {
+		if (lpparam.packageName.equals("com.android.apps.tag") || lpparam.packageName.equals("com.google.android.tag")) {
 			doHooks(lpparam);
 		}
 	}
@@ -21,6 +21,9 @@ public class XposedMod implements IXposedHookLoadPackage {
 		Class<?> TagViewer = XposedHelpers.findClass("com.android.apps.tag.TagViewer", lpparam.classLoader);
 		final Class<?> NdefMessageParser =
 				XposedHelpers.findClass("com.android.apps.tag.message.NdefMessageParser", lpparam.classLoader);
+		
+		final Class<?> UriRecord = XposedHelpers.findClass("com.android.apps.tag.record.UriRecord", lpparam.classLoader);
+		
 		XposedHelpers.findAndHookMethod(TagViewer, "buildTagViews", NdefMessage.class, new XC_MethodHook() {
 			@Override
 			protected void afterHookedMethod(MethodHookParam param) throws Throwable {
@@ -30,14 +33,20 @@ public class XposedMod implements IXposedHookLoadPackage {
 					activity.finish();
 					return;
 				}
-
+				
 				NdefMessage message = (NdefMessage) parameter;		
 				Object parsedMsg = XposedHelpers.callStaticMethod(NdefMessageParser, "parse", message);
 				List<?> records = (List<?>) XposedHelpers.callMethod(parsedMsg, "getRecords");
 				final int size = records.size();
-				if (size == 0) {
-					activity.finish();
-					return;
+				if(size == 1){
+					Object record = UriRecord.cast(records.get(0));
+					Object uri = XposedHelpers.callMethod(record, "getUri");
+					
+					if(uri != null && ((Uri)uri).toString().equals("vnd.android.nfc://ext/motorola.com:xring")){
+						activity.finish();
+						return;
+					}
+					
 				}
 			}
 		});
